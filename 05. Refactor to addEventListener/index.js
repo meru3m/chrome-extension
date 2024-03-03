98248567fa6391fa817f2d48925d227720771e98
meru3m <gustokomagprogram@gmail.com>
2024-03-03 13:36:44 +0800

    learned add event listener

--- 05. Refactor to addEventListener/index.js
+++ 05. Refactor to addEventListener/index.js
@@ -1,6 +1,13 @@
-function saveLead() {
-    console.log("Button clicked!")
+let inputBtn = document.getElementById("input-btn")
+
+inputBtn.addEventListener("click", click)
+
+function click() {
+  console.log("Button clicked from addEventListener")
 }
 
+// original code for the add event listener
 
-
+// inputBtn.addEventListener("click", function () {
+//     console.log("Button clicked from addEventListener")
+// })
